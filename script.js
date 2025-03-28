32cde879debdf9df222665ea9a01fa1b52eee925
Elli. <camille.rehbein.ribeiro@escola.pr.gov.br>
2025-03-28 15:25:49 -0300

    kk

---
+++ script.js
@@ -0,0 +1,29 @@
+let display = document.getElementById('display');
+let currentInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    display.value = currentInput;
+}
+
+function appendOperator(operator) {
+    if (currentInput !== '') {
+        currentInput += operator;
+        display.value = currentInput;
+    }
+}
+
+function clearDisplay() {
+    currentInput = '';
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        currentInput = eval(currentInput).toString();
+        display.value = currentInput;
+    } catch (e) {
+        display.value = 'Erro';
+        currentInput = '';
+    }
+}
